fd90627af1ce62e446446617d49f345e13f43503
na0chan-go <nn19960218@gmail.com>
2024-05-21 23:38:40 +0900

    feat: switchを追加

---
+++ conditional/switch.dart
@@ -0,0 +1,16 @@
+void main() {
+  String fruit = 'apple';
+  switch (fruit) {
+    case 'apple':
+      print('りんごです');
+      break;
+    case 'banana':
+      print('バナナです');
+      break;
+    case 'orange':
+      print('オレンジです');
+      break;
+    default:
+      print('その他の果物です');
+  }
+}
