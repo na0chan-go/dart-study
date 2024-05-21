23535d761d90b5e7546cffa6a3e32d87fdf29887
na0chan-go <nn19960218@gmail.com>
2024-05-21 17:42:09 +0900

    feat: コメントを追加

--- hello-world.dart
+++ hello-world.dart
@@ -1,3 +1,14 @@
 void main() {
+  // "Hello, World!"と出力する.
   print('Hello, World!');
+  /*
+  "こんにちは世界!"と出力する.
+  "こんにちは世界!"と出力する.
+  "こんにちは世界!"と出力する.
+  */
+  print('こんにちは世界!');
+  print('こんにちは世界!');
+  print('こんにちは世界!');
+  // "HELLO, WORLD!"と出力する.
+  print("HELLO, WORLD!");
 }
