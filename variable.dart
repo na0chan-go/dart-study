be7047efeafa4695a64ee7545f1c5b732ec5b29a
na0chan-go <nn19960218@gmail.com>
2024-05-21 17:48:39 +0900

    feat: 変数を追加

---
+++ variable.dart
@@ -0,0 +1,6 @@
+void main() {
+  String message = 'Hello, World!';
+  print(message); // Hello, World!と出力する.
+  String helloWorldMessage = 'こんにちは世界!';
+  print(helloWorldMessage); // こんにちは世界!と出力する.
+}
