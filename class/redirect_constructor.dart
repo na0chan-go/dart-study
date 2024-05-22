b1a117d5f455c479e5f6eca6ad879c27dfe00515
na0chan-go <nn19960218@gmail.com>
2024-05-22 22:41:34 +0900

    feat: redirect_constructorを追加

---
+++ class/redirect_constructor.dart
@@ -0,0 +1,21 @@
+class Person {
+  String name;
+  int age;
+
+  // コンストラクタ
+  Person(this.name, this.age);
+  // 名前付きコンストラクタ
+  Person.fromJSON(Map<String, dynamic> json)
+      : this(json['name'], json['age']); // Redirect constructor
+
+  void greeting() {
+    print('Hello, I am $name, $age years old.');
+  }
+}
+
+void main() {
+  final Person person = Person('Alice', 20);
+  person.greeting(); // Hello, I am Alice, 20 years old.
+  final Person bob = Person.fromJSON({'name': 'Bob', 'age': 25});
+  bob.greeting(); // Hello, I am Bob, 25 years old.
+}
